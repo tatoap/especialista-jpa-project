0b115a5727054c304a49cc72695373cdfdb198d1
Renato <tato_ap@hotmail.com>
2021-07-17 00:31:16 -0300

    Mapeando a primeira entidade com JPA

---
+++ src/main/java/com/algaworks/ecommerce/model/Produto.java
@@ -0,0 +1,65 @@
+package com.algaworks.ecommerce.model;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Produto {
+
+	private Integer id;
+
+	private String nome;
+
+	private String descricao;
+
+	private BigDecimal preco;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
