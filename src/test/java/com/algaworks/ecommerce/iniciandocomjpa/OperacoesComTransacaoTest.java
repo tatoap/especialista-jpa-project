92066931cc171ae35962ccd5cf3cb59f454d7803
Renato <tato_ap@hotmail.com>
2021-07-21 16:59:40 -0300

    Inserindo o primeiro objeto com o método persist

--- src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
+++ src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
@@ -1,11 +1,34 @@
 package com.algaworks.ecommerce.iniciandocomjpa;
 
+import java.math.BigDecimal;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Produto;
 
 public class OperacoesComTransacaoTest extends EntityManagerTest {
+	
+	@Test
+	public void inserirPrimeiroObjeto() {
+		Produto produto = new Produto();
+		
+		produto.setId(2);
+		produto.setNome("Câmera Canon");
+		produto.setDescricao("A melhor definição para as suas fotos!");
+		produto.setPreco(new BigDecimal(5000));
+		
+		entityManager.getTransaction().begin();
+		entityManager.persist(produto);
+		entityManager.getTransaction().commit();
+		
+		entityManager.clear();
+		
+		Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+		Assert.assertNotNull(produtoVerificacao);
+		
+	}
 
 	@Test
 	public void abrirFecharTransacao() {
