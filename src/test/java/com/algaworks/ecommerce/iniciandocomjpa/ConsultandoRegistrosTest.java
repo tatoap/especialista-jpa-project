757e460dd3e38bd71755f72dccba2114b1e423a7
Renato <tato_ap@hotmail.com>
2021-07-20 23:10:11 -0300

    Buscando objetos por identificador

--- src/test/java/com/algaworks/ecommerce/iniciandocomjpa/ConsultandoRegistrosTest.java
+++ src/test/java/com/algaworks/ecommerce/iniciandocomjpa/ConsultandoRegistrosTest.java
@@ -6,8 +6,12 @@ import javax.persistence.Persistence;
 
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.algaworks.ecommerce.model.Produto;
 
 public class ConsultandoRegistrosTest {
 	
@@ -33,5 +37,24 @@ public class ConsultandoRegistrosTest {
 	@After
 	public void tearDown() {
 		entityManager.close();
+	}
+	
+	@Test
+	public void buscarPorIdentificador() {
+		Produto produto = entityManager.find(Produto.class, 1);
+		//Produto produto = entityManager.getReference(Produto.class, 1);
+		
+		Assert.assertNotNull(produto);
+		Assert.assertEquals("Kindle", produto.getNome());
+	}
+	
+	@Test
+	public void atualizarReferencia() {
+		Produto produto = entityManager.find(Produto.class, 1);
+		produto.setNome("Microfone Samson");
+		
+		entityManager.refresh(produto);
+		
+		Assert.assertEquals("Kindle", produto.getNome());
 	}
 }
