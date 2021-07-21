7a7484c2249357442be4e467c128b7653a2b80d4
Renato <tato_ap@hotmail.com>
2021-07-20 23:18:18 -0300

    Criando uma classe genérica para testes

--- src/test/java/com/algaworks/ecommerce/iniciandocomjpa/ConsultandoRegistrosTest.java
+++ src/test/java/com/algaworks/ecommerce/iniciandocomjpa/ConsultandoRegistrosTest.java
@@ -1,43 +1,12 @@
 package com.algaworks.ecommerce.iniciandocomjpa;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Produto;
 
-public class ConsultandoRegistrosTest {
-	
-	private static EntityManagerFactory entityManagerFactory;
-	
-	private EntityManager entityManager;
-	
-	@BeforeClass
-	public static void setUpBeforeClass() {
-		entityManagerFactory = Persistence.createEntityManagerFactory("Ecommerce-PU");
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() {
-		entityManagerFactory.close();
-	}
-	
-	@Before
-	public void setUp() {
-		entityManager = entityManagerFactory.createEntityManager();
-	}
-	
-	@After
-	public void tearDown() {
-		entityManager.close();
-	}
+public class ConsultandoRegistrosTest extends EntityManagerTest {
 	
 	@Test
 	public void buscarPorIdentificador() {
