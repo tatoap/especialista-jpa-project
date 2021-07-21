d75c9a839a1bb74fffffae29a4855bc69010d34d
Renato <tato_ap@hotmail.com>
2021-07-20 22:32:04 -0300

    Montando a classe para testes com o JUnit

---
+++ src/main/java/com/algaworks/ecommerce/util/IniciarUnidadeDePersistencia.java
@@ -0,0 +1,23 @@
+package com.algaworks.ecommerce.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.algaworks.ecommerce.model.Produto;
+
+public class IniciarUnidadeDePersistencia {
+	
+	public static void main(String[] args) {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Ecommerce-PU");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		Produto produto = entityManager.find(Produto.class, 1);
+		System.out.println(produto.getNome());
+		System.out.println(produto.getDescricao());
+		
+		entityManager.close();
+		entityManagerFactory.close();
+	}
+
+}
