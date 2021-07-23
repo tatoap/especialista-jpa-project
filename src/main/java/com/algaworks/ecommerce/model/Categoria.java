16176535e2cc4c77643dbda464c664f1e678fe93
Renato <tato_ap@hotmail.com>
2021-07-22 23:02:39 -0300

    Conhecendo as estratégias para geração de identificador com @GeneratedValue

--- src/main/java/com/algaworks/ecommerce/model/Categoria.java
+++ src/main/java/com/algaworks/ecommerce/model/Categoria.java
@@ -2,6 +2,8 @@ package com.algaworks.ecommerce.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -18,6 +20,7 @@ public class Categoria {
 
 	@Id
 	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 	
 	private String nome;
