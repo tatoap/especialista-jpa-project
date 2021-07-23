ff40299f6cf93c027782f940e36f891acf9d05fe
Renato <tato_ap@hotmail.com>
2021-07-22 23:21:03 -0300

    Configurando a geração de identificador com @TableGenerator

--- src/main/java/com/algaworks/ecommerce/model/Categoria.java
+++ src/main/java/com/algaworks/ecommerce/model/Categoria.java
@@ -5,8 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.TableGenerator;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -21,8 +21,13 @@ public class Categoria {
 
 	@Id
 	@EqualsAndHashCode.Include
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")
-	@SequenceGenerator(name = "seq", sequenceName = "sequencia_chave_primaria") // sequenceName deve ser diferente para cada entidade que usar esse gerador de id
+	@GeneratedValue(strategy = GenerationType.TABLE, generator = "tabela")
+	@TableGenerator(name = "tabela", table = "hibernate_sequences", 
+					pkColumnName = "sequence_name",
+					pkColumnValue = "categoria",
+					valueColumnName = "next_val",
+					initialValue = 0,
+					allocationSize = 50)
 	private Integer id;
 	
 	private String nome;
