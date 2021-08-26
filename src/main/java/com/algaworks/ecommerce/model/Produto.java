ea5e3f7615eefb21372deadc851fa6ce43c691f5
Renato <tato_ap@hotmail.com>
2021-08-26 15:58:12 -0300

    Conhecendo detalhes da anotação @Column

--- src/main/java/com/algaworks/ecommerce/model/Produto.java
+++ src/main/java/com/algaworks/ecommerce/model/Produto.java
@@ -1,8 +1,10 @@
 package com.algaworks.ecommerce.model;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
@@ -32,6 +34,12 @@ public class Produto {
 	@EqualsAndHashCode.Include
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column(name = "data_criacao", updatable = false)
+	private LocalDateTime dataCriacao;
+	
+	@Column(name = "data_ultima_atualizacao", insertable = false)
+	private LocalDateTime dataUltimaAtualizacao;
 
 	private String nome;
 
--- target/classes/META-INF/banco-de-dados/dados-iniciais.sql
+++ target/classes/META-INF/banco-de-dados/dados-iniciais.sql
@@ -1,5 +1,5 @@
-insert into produto (id, nome, preco, descricao) values (1, 'Kindle', 499.0, 'Conheça o novo Kindle, agora com iluminação embutida ajustável, que permite que você leia em ambientes abertos ou fechados, a qualquer hora do dia.');
-insert into produto (id, nome, preco, descricao) values (3, 'Câmera GoPro Hero 7', 1400.0, 'Desempenho 2x melhor.');
+insert into produto (id, nome, preco, data_criacao, descricao) values (1, 'Kindle', 499.0, date_sub(sysdate(), interval 1 day), 'Conheça o novo Kindle, agora com iluminação embutida ajustável, que permite que você leia em ambientes abertos ou fechados, a qualquer hora do dia.');
+insert into produto (id, nome, preco, data_criacao, descricao) values (3, 'Câmera GoPro Hero 7', 1400.0, date_sub(sysdate(), interval 1 day), 'Desempenho 2x melhor.');
 
 insert into cliente (id, nome, sexo) values (1, 'Renato Ramos', 'MASCULINO');
 insert into cliente (id, nome, sexo) values (2, 'Patricia Avelar', 'FEMININO');
