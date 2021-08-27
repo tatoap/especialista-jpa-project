c8ff518a06477bee3c1bfeb0ff67a063879053b2
Renato <tato_ap@hotmail.com>
2021-08-26 21:52:04 -0300

    Mapeando chave primária e estrangeira na mesma coluna com @MapsId

--- src/main/java/com/algaworks/ecommerce/model/ItemPedido.java
+++ src/main/java/com/algaworks/ecommerce/model/ItemPedido.java
@@ -7,6 +7,7 @@ import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
 import javax.persistence.Table;
 
 import lombok.EqualsAndHashCode;
@@ -20,16 +21,17 @@ import lombok.Setter;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class ItemPedido {
 	
-
 	@EmbeddedId
 	private ItemPedidoId id;
 	
+	@MapsId("pedidoId")
 	@ManyToOne(optional = false)
-	@JoinColumn(name = "pedido_id", insertable = false, updatable = false)
+	@JoinColumn(name = "pedido_id")
 	private Pedido pedido;
 	
+	@MapsId("produtoId")
 	@ManyToOne(optional = false)
-	@JoinColumn(name = "produto_id", insertable = false, updatable = false)
+	@JoinColumn(name = "produto_id")
 	private Produto produto;
 	
 	@Column(name = "preco_produto")
