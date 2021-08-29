bd5f0870d7eab78b025d39aee72b089301fe16e9
Renato <tato_ap@hotmail.com>
2021-08-29 19:03:07 -0300

    Configurando colunas com @Column

--- src/main/java/com/algaworks/ecommerce/model/Produto.java
+++ src/main/java/com/algaworks/ecommerce/model/Produto.java
@@ -38,10 +38,13 @@ public class Produto extends EntidadeBaseInteger {
 	@Column(name = "data_ultima_atualizacao", insertable = false)
 	private LocalDateTime dataUltimaAtualizacao;
 
+	@Column(length = 100, nullable = false) // nome varchar(100) not null
 	private String nome;
 
+	@Column(columnDefinition = "varchar(275) not null default 'descrição'")
 	private String descricao;
 
+	@Column(precision = 10, scale = 2) // preco deciman(10,2)
 	private BigDecimal preco;
 	
 	@Lob
