205e5de275cdc10222c51ff49d45f19e833dbe30
Renato <tato_ap@hotmail.com>
2021-09-18 19:18:25 -0300

    Selecionando um atributo da entidade como retorno da consulta

--- src/test/java/com/algaworks/ecommerce/iniciandocomjpa/ConsultandoRegistrosTest.java
+++ src/test/java/com/algaworks/ecommerce/iniciandocomjpa/ConsultandoRegistrosTest.java
@@ -1,12 +1,32 @@
 package com.algaworks.ecommerce.iniciandocomjpa;
 
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Cliente;
 import com.algaworks.ecommerce.model.Produto;
 
 public class ConsultandoRegistrosTest extends EntityManagerTest {
+	
+	@Test
+	public void selecionarUmAtributoParaRetorno() {
+		String jpql = "select p.nome from Produto p";
+		
+		TypedQuery<String> typedQuery = entityManager.createQuery(jpql, String.class);
+		List<String> lista = typedQuery.getResultList();
+		Assert.assertTrue(String.class.equals(lista.get(0).getClass()));
+		
+		String jpqlCliente = "select p.cliente from Pedido p";
+		
+		TypedQuery<Cliente> typedQueryCliente = entityManager.createQuery(jpqlCliente, Cliente.class);
+		List<Cliente> listaCliente = typedQueryCliente.getResultList();
+		Assert.assertTrue(Cliente.class.equals(listaCliente.get(0).getClass()));
+	}
 	
 	@Test
 	public void buscarPorIdentificador() {
