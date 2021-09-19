b3946dc2675827186078f2aedff037f8e9d864b1
Renato <tato_ap@hotmail.com>
2021-09-19 17:49:04 -0300

    Entendendo as Path Expressions

---
+++ src/test/java/com/algaworks/ecommerce/jpql/PathExpressionTest.java
@@ -0,0 +1,24 @@
+package com.algaworks.ecommerce.jpql;
+
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+
+public class PathExpressionTest extends EntityManagerTest {
+
+	@Test
+	public void usarPathExpressions() {
+		String jpql = "select p.cliente.nome from Pedido p";
+		
+		TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+		List<Object[]> lista = typedQuery.getResultList();
+		
+		Assert.assertFalse(lista.isEmpty());
+	}
+	
+}
