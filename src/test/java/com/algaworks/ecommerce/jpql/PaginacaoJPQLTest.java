973e80235d39c397ea11f76f33c52f96bb68e988
Renato <tato_ap@hotmail.com>
2021-09-20 12:41:23 -0300

    Fazendo paginação de resultados

---
+++ src/test/java/com/algaworks/ecommerce/jpql/PaginacaoJPQLTest.java
@@ -0,0 +1,32 @@
+package com.algaworks.ecommerce.jpql;
+
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Categoria;
+
+public class PaginacaoJPQLTest extends EntityManagerTest {
+	
+	@Test
+	public void paginarResultados() {
+		String jpql = "select c from Categoria c order by c.nome";
+		
+		TypedQuery<Categoria> typedQuery = entityManager.createQuery(jpql, Categoria.class);
+		
+		//FIRST_RESULT = MAX_RESULT * (pagina - 1)
+		typedQuery.setFirstResult(6);
+		typedQuery.setMaxResults(2);
+		
+		List<Categoria> lista = typedQuery.getResultList();
+		
+		Assert.assertFalse(lista.isEmpty());
+		
+		lista.forEach(c -> System.out.println(c.getId() + ", " + c.getNome()));
+	}
+
+}
