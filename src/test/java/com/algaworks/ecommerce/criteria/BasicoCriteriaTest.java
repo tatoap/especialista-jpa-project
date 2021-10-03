6faee567061a29ebf4eedd5362f66f3f4f16846a
Renato <tato_ap@hotmail.com>
2021-10-02 22:42:18 -0300

    Selecionando um atributo da entidade como retorno da consulta

--- src/test/java/com/algaworks/ecommerce/criteria/BasicoCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criteria/BasicoCriteriaTest.java
@@ -1,5 +1,8 @@
 package com.algaworks.ecommerce.criteria;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -10,8 +13,46 @@ import org.junit.Test;
 
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.Produto;
 
 public class BasicoCriteriaTest extends EntityManagerTest {
+	
+	@Test
+	public void retornarTodosOsProdutos() {
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Produto> criteriaQuery = criteriaBuilder.createQuery(Produto.class);
+		Root<Produto> root = criteriaQuery.from(Produto.class);
+		
+		criteriaQuery.select(root);
+		
+		TypedQuery<Produto> typedQuery = entityManager.createQuery(criteriaQuery);
+		List<Produto> lista = typedQuery.getResultList();
+		
+		Assert.assertFalse(lista.isEmpty());
+	}
+	
+	@Test
+	public void selecionarUmAtributoParaRetorno() {
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		
+		//CriteriaQuery<Cliente> criteriaQuery = criteriaBuilder.createQuery(Cliente.class);
+		CriteriaQuery<BigDecimal> criteriaQuery = criteriaBuilder.createQuery(BigDecimal.class);
+		Root<Pedido> root = criteriaQuery.from(Pedido.class);
+		
+		//criteriaQuery.select(root.get("cliente")); // a mesma coisa que "select p.cliente from Pedido p where p.id = 1"
+		criteriaQuery.select(root.get("total"));
+		
+		criteriaQuery.where(criteriaBuilder.equal(root.get("id"), 1));
+		
+		//TypedQuery<Cliente> typedQuery = entityManager.createQuery(criteriaQuery);
+		TypedQuery<BigDecimal> typedQuery = entityManager.createQuery(criteriaQuery);
+		
+		//Cliente cliente = typedQuery.getSingleResult();
+		BigDecimal total = typedQuery.getSingleResult();
+		
+		//Assert.assertEquals("Renato Ramos", cliente.getNome());
+		Assert.assertEquals(new BigDecimal("2398.00"), total);
+	}
 	
 	@Test
 	public void buscarPorIdentificador() {
