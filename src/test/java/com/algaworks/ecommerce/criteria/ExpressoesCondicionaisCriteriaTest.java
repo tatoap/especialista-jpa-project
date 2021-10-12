5f04c9ae7025a40ac3506a032952a2f74938143c
Renato <tato_ap@hotmail.com>
2021-10-11 21:41:28 -0300

    Usando expressão de diferente

--- src/test/java/com/algaworks/ecommerce/criteria/ExpressoesCondicionaisCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criteria/ExpressoesCondicionaisCriteriaTest.java
@@ -23,6 +23,25 @@ import com.algaworks.ecommerce.model.Produto_;
 public class ExpressoesCondicionaisCriteriaTest extends EntityManagerTest {
 	
 	@Test
+	public void usarExpressaoDiferente() {
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+		Root<Pedido> root = criteriaQuery.from(Pedido.class);
+		
+		criteriaQuery.select(root);
+		
+		criteriaQuery.where(criteriaBuilder.notEqual(root.get(Pedido_.total), new BigDecimal(499)));
+		
+		TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+		List<Pedido> lista = typedQuery.getResultList();
+		Assert.assertFalse(lista.isEmpty());
+		
+		lista.stream()
+		.forEach(p -> 
+			System.out.println("ID: " + p.getId() + ", Cliente: " + p.getCliente().getNome() + ", Total: " + p.getTotal()));
+	}
+	
+	@Test
 	public void usarBetween() {
 		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
 		CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
