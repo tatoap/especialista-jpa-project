d155516ff4f1689b86b6f12c724c6c676135c52d
Renato <tato_ap@hotmail.com>
2021-10-14 12:08:22 -0300

    Usando funções para string

---
+++ src/test/java/com/algaworks/ecommerce/criteria/FuncoesCriteriaTest.java
@@ -0,0 +1,57 @@
+package com.algaworks.ecommerce.criteria;
+
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Cliente;
+import com.algaworks.ecommerce.model.Cliente_;
+
+public class FuncoesCriteriaTest extends EntityManagerTest {
+
+	@Test
+	public void aplicarFuncoesString() {
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+		Root<Cliente> root = criteriaQuery.from(Cliente.class);
+		
+		criteriaQuery.multiselect(
+				root.get(Cliente_.nome),
+				criteriaBuilder.concat("Nome do cliente: ", root.get(Cliente_.nome)),
+				criteriaBuilder.length(root.get(Cliente_.nome)),
+				criteriaBuilder.locate(root.get(Cliente_.nome), "a"),
+				criteriaBuilder.substring(root.get(Cliente_.nome), 1, 2),
+				criteriaBuilder.lower(root.get(Cliente_.nome)),
+				criteriaBuilder.upper(root.get(Cliente_.nome)),
+				criteriaBuilder.trim(root.get(Cliente_.nome))
+		);
+		
+		criteriaQuery.where(criteriaBuilder.equal(
+				criteriaBuilder.substring(root.get(Cliente_.nome), 1, 1), "P"));
+		
+		TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+		List<Object[]> lista = typedQuery.getResultList();
+		
+		Assert.assertFalse(lista.isEmpty());
+		
+		lista.forEach(arr -> System.out.println(
+				arr[0]
+						+ ", concat: " + arr[1]
+						+ ", length: " + arr[2]
+						+ ", locate: " + arr[3]
+						+ ", substring: " + arr[4]
+						+ ", lower: " + arr[5]
+						+ ", upper: " + arr[6]
+						+ ", trim: |" + arr[7] + "|"
+		));
+		
+	}
+	
+}
