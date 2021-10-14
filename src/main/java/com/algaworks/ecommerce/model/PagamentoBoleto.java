15469c6e75397eae2ba64b824fb15b5fb27dcadd
Renato <tato_ap@hotmail.com>
2021-10-14 12:53:02 -0300

    Usando funções para datas

--- src/main/java/com/algaworks/ecommerce/model/PagamentoBoleto.java
+++ src/main/java/com/algaworks/ecommerce/model/PagamentoBoleto.java
@@ -1,5 +1,7 @@
 package com.algaworks.ecommerce.model;
 
+import java.time.LocalDate;
+
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
@@ -16,5 +18,8 @@ public class PagamentoBoleto extends Pagamento {
 	
 	@Column(name = "codigo_barras", length = 100)
 	private String codigoBarras;
+	
+	@Column(name = "data_vencimento")
+	private LocalDate dataVencimento;
 
 }
--- src/test/java/com/algaworks/ecommerce/criteria/FuncoesCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criteria/FuncoesCriteriaTest.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Root;
 
 import org.junit.Assert;
@@ -13,8 +14,52 @@ import org.junit.Test;
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Cliente;
 import com.algaworks.ecommerce.model.Cliente_;
+import com.algaworks.ecommerce.model.Pagamento;
+import com.algaworks.ecommerce.model.PagamentoBoleto;
+import com.algaworks.ecommerce.model.PagamentoBoleto_;
+import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.Pedido_;
+import com.algaworks.ecommerce.model.StatusPedido;
 
 public class FuncoesCriteriaTest extends EntityManagerTest {
+	
+	@Test
+	public void aplicarFuncaoData() {
+		// current_date, current_time, current_timestamp
+		
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+		Root<Pedido> root = criteriaQuery.from(Pedido.class);
+		Join<Pedido, Pagamento> joinPagamento = root.join(Pedido_.pagamento);
+		Join<Pedido, PagamentoBoleto> joinPagamentoBoleto = criteriaBuilder
+				.treat(joinPagamento, PagamentoBoleto.class);
+		
+		criteriaQuery.multiselect(
+				root.get(Pedido_.id),
+				criteriaBuilder.currentDate(),
+				criteriaBuilder.currentTime(),
+				criteriaBuilder.currentTimestamp()
+		);
+		
+		criteriaQuery.where(
+				criteriaBuilder.between(criteriaBuilder.currentDate(), 
+						root.get(Pedido_.dataCriacao).as(java.sql.Date.class),
+						joinPagamentoBoleto.get(PagamentoBoleto_.dataVencimento).as(java.sql.Date.class)),
+				criteriaBuilder.equal(root.get(Pedido_.status), StatusPedido.AGUARDANDO)
+		);
+		
+		TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+		List<Object[]> lista = typedQuery.getResultList();
+		
+		Assert.assertFalse(lista.isEmpty());
+		
+		lista.forEach(arr -> System.out.println(
+				arr[0]
+						+ ", current_date: " + arr[1]
+						+ ", current_time: " + arr[2]
+						+ ", current_timestamp: " + arr[3]
+		));
+	}
 
 	@Test
 	public void aplicarFuncoesString() {
