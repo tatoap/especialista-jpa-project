01c6cab208137cea851db861ed436d35adc87d4a
Renato <tato_ap@hotmail.com>
2021-10-14 13:36:46 -0300

    Usando funções para números

--- src/test/java/com/algaworks/ecommerce/criteria/FuncoesCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criteria/FuncoesCriteriaTest.java
@@ -24,6 +24,35 @@ import com.algaworks.ecommerce.model.StatusPedido;
 public class FuncoesCriteriaTest extends EntityManagerTest {
 	
 	@Test
+	public void aplicarFuncaoNumero() {
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+		Root<Pedido> root = criteriaQuery.from(Pedido.class);
+		
+		criteriaQuery.multiselect(
+				root.get(Pedido_.id),
+				criteriaBuilder.abs(criteriaBuilder.prod(root.get(Pedido_.id), -1)),
+				criteriaBuilder.mod(root.get(Pedido_.id), 2),
+				criteriaBuilder.sqrt(root.get(Pedido_.total))
+		);
+		
+		criteriaQuery.where(criteriaBuilder.greaterThan(
+				criteriaBuilder.sqrt(root.get(Pedido_.total)), 10.0));
+		
+		TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+		List<Object[]> lista = typedQuery.getResultList();
+		
+		Assert.assertFalse(lista.isEmpty());
+		
+		lista.forEach(arr -> System.out.println(
+				arr[0]
+						+ ", abs: " + arr[1]
+						+ ", mod: " + arr[2]
+						+ ", sqrt: " + arr[3]
+		));
+	}
+	
+	@Test
 	public void aplicarFuncaoData() {
 		// current_date, current_time, current_timestamp
 		
