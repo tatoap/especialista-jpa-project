a58c82925714ae58c02c0decc32402d66ecd78e7
Renato <tato_ap@hotmail.com>
2021-10-15 13:08:29 -0300

    Exercício: usando group by

--- src/test/java/com/algaworks/ecommerce/criteria/GroupByCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criteria/GroupByCriteriaTest.java
@@ -14,14 +14,44 @@ import org.junit.Test;
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Categoria;
 import com.algaworks.ecommerce.model.Categoria_;
+import com.algaworks.ecommerce.model.Cliente;
+import com.algaworks.ecommerce.model.Cliente_;
 import com.algaworks.ecommerce.model.ItemPedido;
 import com.algaworks.ecommerce.model.ItemPedido_;
+import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.Pedido_;
 import com.algaworks.ecommerce.model.Produto;
 import com.algaworks.ecommerce.model.Produto_;
 
 public class GroupByCriteriaTest extends EntityManagerTest {
-
+	
 	@Test
+	public void agruparResultado03() {
+		//Total de vendas por cliente
+		//String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip " +
+		//		"join ip.pedido p join p.cliente c " +
+		//		"group by c.id";
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+		Root<ItemPedido> root = criteriaQuery.from(ItemPedido.class);
+		Join<ItemPedido, Pedido> joinPedido = root.join(ItemPedido_.pedido);
+		Join<Pedido, Cliente> joinPedidoCliente = joinPedido.join(Pedido_.cliente);
+		
+		criteriaQuery.multiselect(
+				joinPedidoCliente.get(Cliente_.nome), 
+				criteriaBuilder.sum(root.get(ItemPedido_.precoProduto)));
+		
+		criteriaQuery.groupBy(joinPedidoCliente.get(Cliente_.id));
+		
+		TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+		List<Object[]> lista = typedQuery.getResultList();
+		
+		Assert.assertFalse(lista.isEmpty());
+		
+		lista.forEach(arr -> System.out.println("Nome: " + arr[0] + ", Sum: " + arr[1]));
+	}
+
+	//@Test
 	public void agruparResultado02() {
 		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
 		CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
