52631d265049f2749b617498cd9efeddf50462ff
Renato <tato_ap@hotmail.com>
2021-10-23 20:03:36 -0300

    Usando o having para condicionar o agrupamento

--- src/test/java/com/algaworks/ecommerce/criteria/GroupByCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criteria/GroupByCriteriaTest.java
@@ -1,5 +1,6 @@
 package com.algaworks.ecommerce.criteria;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import javax.persistence.TypedQuery;
@@ -27,6 +28,44 @@ import com.algaworks.ecommerce.model.Produto_;
 public class GroupByCriteriaTest extends EntityManagerTest {
 	
 	@Test
+	public void condicionarAgrupamentoComHaving() {
+		// Total de vendas dentre as categorias que mais vendem.
+		// String jpql = "select cat.nome, sum(ip.precoProduto) from ItemPedido ip " +
+		// 		"join ip.produto pro join pro.categorias cat " +
+		//		"group by cat.id " +
+		//		"having sum(ip.precoProduto) > 100";
+		
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+		Root<ItemPedido> root = criteriaQuery.from(ItemPedido.class);
+		Join<ItemPedido, Produto> joinProduto = root.join(ItemPedido_.produto);
+		Join<Produto, Categoria> joinProdutoCategoria = joinProduto.join(Produto_.categorias);
+		
+		criteriaQuery.multiselect(
+				joinProdutoCategoria.get(Categoria_.nome),
+				criteriaBuilder.sum(root.get(ItemPedido_.precoProduto)),
+				criteriaBuilder.avg(root.get(ItemPedido_.precoProduto))
+		);
+		
+		criteriaQuery.groupBy(joinProdutoCategoria.get(Categoria_.id));
+		
+		criteriaQuery.having(criteriaBuilder.greaterThan(
+				criteriaBuilder.avg(
+						root.get(ItemPedido_.precoProduto)).as(BigDecimal.class), 
+				new BigDecimal(700)));
+		
+		TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+		List<Object[]> lista = typedQuery.getResultList();
+		
+		Assert.assertFalse(lista.isEmpty());
+		
+		lista.forEach(arr -> System.out.println(
+				"Nome categoria: "+ arr[0]
+						+ ", SUM: " + arr[1]
+						+ ", AVG: " + arr[2]));
+	}
+	
+	//@Test
 	public void agruparResultadoComFuncoes() {
 		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
 		CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
@@ -45,6 +84,8 @@ public class GroupByCriteriaTest extends EntityManagerTest {
 		
 		TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
 		List<Object[]> lista = typedQuery.getResultList();
+		
+		Assert.assertFalse(lista.isEmpty());
 		
 		lista.forEach(arr -> System.out.println("Ano/Mês: " + arr[0] + ", Sum: " + arr[1]));
 	}
