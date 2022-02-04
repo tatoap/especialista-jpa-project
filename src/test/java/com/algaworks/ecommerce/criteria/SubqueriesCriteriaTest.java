7e8a33f00b17158145fd6d4c27f1fbaffe1aff7b
Renato <tato_ap@hotmail.com>
2022-02-04 17:59:13 -0300

    Exercício: criando subqueries com all

--- src/test/java/com/algaworks/ecommerce/criteria/SubqueriesCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criteria/SubqueriesCriteriaTest.java
@@ -28,6 +28,41 @@ import com.algaworks.ecommerce.model.Produto_;
 public class SubqueriesCriteriaTest extends EntityManagerTest {
 	
 	@Test
+	public void pesquisarComAllExercicio() {
+		//Todos os produtos que sempre foram vendidos pelo mesmo preço
+		//String jpql = "select distinct p from ItemPedido ip join ip.produto p where " +
+		//	" ip.precoProduto = ALL " +
+		//	" (select precoProduto from ItemPedido where produto = p and id <> ip.id)";
+		
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Produto> criteriaQuery = criteriaBuilder.createQuery(Produto.class);
+		Root<ItemPedido> root = criteriaQuery.from(ItemPedido.class);
+		
+		criteriaQuery.select(root.get(ItemPedido_.produto));
+		criteriaQuery.distinct(true);
+		
+		Subquery<BigDecimal> subquery = criteriaQuery.subquery(BigDecimal.class);
+		Root<ItemPedido> subqueryRoot = subquery.from(ItemPedido.class);
+		subquery.select(subqueryRoot.get(ItemPedido_.precoProduto));
+		subquery.where(
+				criteriaBuilder.equal(subqueryRoot.get(ItemPedido_.produto), root.get(ItemPedido_.produto)),
+				criteriaBuilder.notEqual(subqueryRoot, root)
+		);
+		
+		criteriaQuery.where(
+				criteriaBuilder.equal(
+						root.get(ItemPedido_.precoProduto), criteriaBuilder.all(subquery))
+		);
+		
+		TypedQuery<Produto> typedQuery = entityManager.createQuery(criteriaQuery);
+		
+		List<Produto> lista = typedQuery.getResultList();
+		Assert.assertFalse(lista.isEmpty());
+		
+		lista.forEach(obj -> System.out.println("ID: " + obj.getId()));
+	}
+	
+	//@Test
 	public void pesquisarComAny02() {
 		//Todos os produtos que já foram vendidos por um preco diferente do atual.
 		//String jpql = "select p from Produto p where " +
@@ -55,7 +90,7 @@ public class SubqueriesCriteriaTest extends EntityManagerTest {
 		lista.forEach(obj -> System.out.println("ID: " + obj.getId()));
 	}
 	
-	@Test
+	//@Test
 	public void pesquisarComAny01() {
 		//Todos os produtos que já foram vendidos, pelo menos, uma vez pelo preco atual.
 		//String jpql = "select p from Produto p where " +
