655408090e17c87ee7d976fe8d094b3bf4658149
Renato <tato_ap@hotmail.com>
2022-02-05 18:58:08 -0300

    Executando SQL e retornando uma entidade

--- src/test/java/com/algaworks/ecommerce/consultasnativas/ConsultaNativaTest.java
+++ src/test/java/com/algaworks/ecommerce/consultasnativas/ConsultaNativaTest.java
@@ -7,10 +7,34 @@ import javax.persistence.Query;
 import org.junit.Test;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Produto;
 
 public class ConsultaNativaTest extends EntityManagerTest {
 	
 	@Test
+	public void executarSQLRetornandoEntidade() {
+		String sql = "select id, nome, descricao, data_criacao, data_ultima_atualizacao, preco, fotoProduto " +
+					 "from produto_loja";
+
+		//String sql = "select prd_id id, prd_nome nome, prd_descricao descricao, " +
+		//      	   "prd_data_criacao data_criacao, prd_data_ultima_atualizacao data_ultima_atualizacao, " +
+		//      	   "prd_preco preco, prd_foto foto " +
+		//      	   "from ecm_produto";
+		
+		//String sql = "select id, nome, descricao, null data_criacao, " +
+		//			 "null data_ultima_atualizacao, " +
+		//			 "preco, null foto_produto " +
+		//			 "from erp_produto";
+		
+		Query query = entityManager.createNativeQuery(sql, Produto.class);
+		
+		List<Produto> lista = query.getResultList();
+		
+		lista.stream().forEach(obj -> System.out.println(
+				String.format("Produto => ID: %s, Nome: %s ", obj.getId(), obj.getNome())));
+	}
+	
+	//@Test
 	public void executarSQL() {
 		String sql = "select id, nome from produto order by id";
 		Query query = entityManager.createNativeQuery(sql);
