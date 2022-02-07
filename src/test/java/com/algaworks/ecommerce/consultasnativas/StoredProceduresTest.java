8bd4f5e1bc95322f6bd9c534fdb76794a02de69b
Renato <tato_ap@hotmail.com>
2022-02-07 15:27:56 -0300

    Recebendo uma lista de registros da procedure

--- src/test/java/com/algaworks/ecommerce/consultasnativas/StoredProceduresTest.java
+++ src/test/java/com/algaworks/ecommerce/consultasnativas/StoredProceduresTest.java
@@ -1,5 +1,7 @@
 package com.algaworks.ecommerce.consultasnativas;
 
+import java.util.List;
+
 import javax.persistence.ParameterMode;
 import javax.persistence.StoredProcedureQuery;
 
@@ -7,10 +9,25 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Cliente;
 
 public class StoredProceduresTest extends EntityManagerTest {
-
+	
 	@Test
+	public void receberListaDaProcedure() {
+		StoredProcedureQuery storedProcedureQuery = entityManager
+				.createStoredProcedureQuery("compraram_acima_media", Cliente.class);
+		
+		storedProcedureQuery.registerStoredProcedureParameter("ano", Integer.class, ParameterMode.IN);
+		
+		storedProcedureQuery.setParameter("ano", 2022);
+		
+		List<Cliente> lista = storedProcedureQuery.getResultList();
+		
+		Assert.assertFalse(lista.isEmpty());
+	}
+
+	//@Test
 	public void usarParametrosInEOut() {
 		StoredProcedureQuery storedProcedureQuery = entityManager
 				.createStoredProcedureQuery("buscar_nome_produto");
