607eb19cb398cc1b44f327036735d5ec0456b1bb
Renato <tato_ap@hotmail.com>
2022-02-07 18:25:42 -0300

    Invocando uma view do banco de dados

--- target/classes/META-INF/banco-de-dados/script-remocao.sql
+++ target/classes/META-INF/banco-de-dados/script-remocao.sql
@@ -15,3 +15,5 @@ drop procedure if exists buscar_nome_produto;
 drop procedure if exists compraram_acima_media;
 
 drop procedure if exists ajustar_preco_produto;
+
+drop view if exists view_produto;
