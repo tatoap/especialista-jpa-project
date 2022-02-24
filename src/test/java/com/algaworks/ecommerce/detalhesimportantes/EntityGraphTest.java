47f010d905ed0f4c5fd40b991b6caf077f5a1252
Renato <tato_ap@hotmail.com>
2022-02-24 13:55:34 -0300

    Adicionando um Subgraph na consulta

--- src/test/java/com/algaworks/ecommerce/detalhesimportantes/EntityGraphTest.java
+++ src/test/java/com/algaworks/ecommerce/detalhesimportantes/EntityGraphTest.java
@@ -3,17 +3,33 @@ package com.algaworks.ecommerce.detalhesimportantes;
 import java.util.List;
 
 import javax.persistence.EntityGraph;
+import javax.persistence.Subgraph;
 import javax.persistence.TypedQuery;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Cliente;
 import com.algaworks.ecommerce.model.Pedido;
 
 public class EntityGraphTest extends EntityManagerTest {
 	
 	@Test
+	public void buscarAtributosEssenciaisPedido02() {
+		EntityGraph<Pedido> entityGraph = entityManager.createEntityGraph(Pedido.class);
+		entityGraph.addAttributeNodes("dataCriacao", "status", "total");
+		
+		Subgraph<Cliente> subgraphCliente = entityGraph.addSubgraph("cliente", Cliente.class);
+		subgraphCliente.addAttributeNodes("nome", "cpf");
+		
+		TypedQuery<Pedido> typedQuery = entityManager.createQuery("select p from Pedido p", Pedido.class);
+        typedQuery.setHint("javax.persistence.fetchgraph", entityGraph);
+        List<Pedido> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+	}
+	
+	//@Test
 	public void buscarAtributosEssenciaisPedido() {
 		EntityGraph<Pedido> entityGraph = entityManager.createEntityGraph(Pedido.class);
 		entityGraph.addAttributeNodes("dataCriacao", "status", "total", "notaFiscal");
