9820ddb49b0026d3e6afeaaeb946d9dddc13f511
Renato <tato_ap@hotmail.com>
2022-09-02 18:03:09 -0300

    Concorrência e locking

--- src/main/java/com/algaworks/ecommerce/model/EntidadeBaseInteger.java
+++ src/main/java/com/algaworks/ecommerce/model/EntidadeBaseInteger.java
@@ -4,6 +4,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.Version;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -19,5 +20,8 @@ public class EntidadeBaseInteger {
 	@EqualsAndHashCode.Include
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Version
+    private Integer versao;
 	
 }
